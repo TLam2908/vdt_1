cf08073054b6a7ad6b8dac5eebf30f59cbda428d
TLam2908 <buitunglam2003@gmail.com>
2025-05-30 03:19:12 +0700

    add: add image for products

---
+++ client/src/pages/productPage.tsx
@@ -0,0 +1,10 @@
+const ProductPage = () => {
+    return (
+        <div>
+            <h1>Product Page</h1>
+            <p>This is the product page where you can find details about our products.</p>
+        </div>
+    )
+}
+
+export default ProductPage; 
