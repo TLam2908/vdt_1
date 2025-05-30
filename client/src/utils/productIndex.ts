e837f092bae4d22249754033edd011ebc6c89a19
TLam2908 <buitunglam2003@gmail.com>
2025-05-30 11:00:16 +0700

    feat: create productIndex

---
+++ client/src/utils/productIndex.ts
@@ -0,0 +1,283 @@
+// Hero images and text for different iPhone models
+
+import hero16 from "../assets/product/hero/ip16/hero.png";
+import hero16Text from "../assets/product/hero/ip16/hero_text.png";
+import hero16Small from "../assets/product/hero/ip16/hero_small.png";
+import hero16TextSmall from "../assets/product/hero/ip16/hero_text_small.png";
+import hero16Pro from "../assets/product/hero/ip16pro/hero.png";
+import hero16ProText from "../assets/product/hero/ip16pro/hero_text.png";
+import hero16ProSmall from "../assets/product/hero/ip16pro/hero_small.png";
+import hero16ProTextSmall from "../assets/product/hero/ip16pro/hero_text_small.png";
+import hero16e from "../assets/product/hero/ip16e/hero.png";
+import hero16eSmall from "../assets/product/hero/ip16e/hero_small.png";
+
+export const productHeroImages = {
+    ip16: {
+        hero: hero16,
+        text: hero16Text,
+        small: hero16Small,
+        textSmall: hero16TextSmall,
+    },
+    ip16Pro: {
+        hero: hero16Pro,
+        text: hero16ProText,
+        small: hero16ProSmall,
+        textSmall: hero16ProTextSmall,
+    },
+    ip16e: {
+        hero: hero16e,
+        small: hero16eSmall,
+    },
+}
+
+
+// Hightlight images and text for different iPhone models
+import highlight16_1 from "../assets/product/highlight/ip16/1/xlarge.jpg";
+import highlight16_2 from "../assets/product/highlight/ip16/2/xlarge.jpg";
+import highlight16_3 from "../assets/product/highlight/ip16/3/xlarge.jpg";
+import highlight16_4 from "../assets/product/highlight/ip16/4/xlarge.jpg";
+import highlight16_5 from "../assets/product/highlight/ip16/5/xlarge.jpg";
+import highlight16_1_small from "../assets/product/highlight/ip16/1/small.jpg";
+import highlight16_2_small from "../assets/product/highlight/ip16/2/small.jpg"; 
+import highlight16_3_small from "../assets/product/highlight/ip16/3/small.jpg";
+import highlight16_4_small from "../assets/product/highlight/ip16/4/small.jpg";
+import highlight16_5_small from "../assets/product/highlight/ip16/5/small.jpg";
+
+import highlight16Pro_1 from "../assets/product/highlight/ip16pro/1/xlarge.jpg";
+import highlight16Pro_2 from "../assets/product/highlight/ip16pro/2/xlarge.jpg";
+import highlight16Pro_3 from "../assets/product/highlight/ip16pro/3/xlarge.jpg";
+import highlight16Pro_4 from "../assets/product/highlight/ip16pro/4/xlarge.jpg";
+import highlight16Pro_5 from "../assets/product/highlight/ip16pro/5/xlarge.jpg";
+import highlight16Pro_6 from "../assets/product/highlight/ip16pro/6/xlarge.jpg";
+import highlight16Pro_1_small from "../assets/product/highlight/ip16pro/1/small.jpg";
+import highlight16Pro_2_small from "../assets/product/highlight/ip16pro/2/small.jpg";
+import highlight16Pro_3_small from "../assets/product/highlight/ip16pro/3/small.jpg";
+import highlight16Pro_4_small from "../assets/product/highlight/ip16pro/4/small.jpg";
+import highlight16Pro_5_small from "../assets/product/highlight/ip16pro/5/small.jpg";
+import highlight16Pro_6_small from "../assets/product/highlight/ip16pro/6/small.jpg";
+
+import highlight16e_1 from "../assets/product/highlight/ip16e/1/xlarge.jpg";
+import highlight16e_2 from "../assets/product/highlight/ip16e/2/xlarge.jpg";
+import highlight16e_3 from "../assets/product/highlight/ip16e/3/xlarge.jpg";
+import highlight16e_4 from "../assets/product/highlight/ip16e/4/xlarge.jpg";
+import highlight16e_5 from "../assets/product/highlight/ip16e/5/xlarge.jpg";
+import highlight16e_1_small from "../assets/product/highlight/ip16e/1/small.jpg";
+import highlight16e_2_small from "../assets/product/highlight/ip16e/2/small.jpg";
+import highlight16e_3_small from "../assets/product/highlight/ip16e/3/small.jpg";
+import highlight16e_4_small from "../assets/product/highlight/ip16e/4/small.jpg";
+import highlight16e_5_small from "../assets/product/highlight/ip16e/5/small.jpg";
+
+export const productHighlightImages = {
+    ip16: {
+        images: [
+            highlight16_1,
+            highlight16_2,
+            highlight16_3,
+            highlight16_4,
+            highlight16_5
+        ],
+        smallImages: [
+            highlight16_1_small,
+            highlight16_2_small,
+            highlight16_3_small,
+            highlight16_4_small,
+            highlight16_5_small
+        ]
+    },
+    ip16Pro: {
+        images: [
+            highlight16Pro_1,
+            highlight16Pro_2,
+            highlight16Pro_3,
+            highlight16Pro_4,
+            highlight16Pro_5,
+            highlight16Pro_6
+        ],
+        smallImages: [
+            highlight16Pro_1_small,
+            highlight16Pro_2_small,
+            highlight16Pro_3_small,
+            highlight16Pro_4_small,
+            highlight16Pro_5_small,
+            highlight16Pro_6_small
+        ]
+    },
+    ip16e: {
+        images: [
+            highlight16e_1,
+            highlight16e_2,
+            highlight16e_3,
+            highlight16e_4,
+            highlight16e_5
+        ],
+        smallImages: [
+            highlight16e_1_small,
+            highlight16e_2_small,
+            highlight16e_3_small,
+            highlight16e_4_small,
+            highlight16e_5_small
+        ]
+    }
+}
+
+// Product details for different iPhone models
+import detail16_1 from "../assets/product/detail/ip16/1/xlarge.jpg";
+import detail16_2 from "../assets/product/detail/ip16/2/xlarge.jpg";
+import detail16_3 from "../assets/product/detail/ip16/3/xlarge.jpg";
+import detail16_4 from "../assets/product/detail/ip16/4/xlarge.jpg";
+
+import detail16Pro_1 from "../assets/product/detail/ip16pro/1/xlarge.jpg";
+import detail16Pro_2 from "../assets/product/detail/ip16pro/2/xlarge.jpg";
+import detail16Pro_3 from "../assets/product/detail/ip16pro/3/xlarge.jpg";
+import detail16Pro_4 from "../assets/product/detail/ip16pro/4/xlarge.jpg";
+
+import detail16e_1 from "../assets/product/detail/ip16e/1/xlarge.jpg";
+import detail16e_2 from "../assets/product/detail/ip16e/2/xlarge.jpg";
+import detail16e_3 from "../assets/product/detail/ip16e/3/xlarge.jpg";
+import detail16e_4 from "../assets/product/detail/ip16e/4/xlarge.jpg";
+import detail16e_5 from "../assets/product/detail/ip16e/5/xlarge.jpg";
+
+export const productDetailImages = {
+    ip16: {
+        images: [
+            detail16_1,
+            detail16_2,
+            detail16_3,
+            detail16_4
+        ]
+    },
+    ip16Pro: {
+        images: [
+            detail16Pro_1,
+            detail16Pro_2,
+            detail16Pro_3,
+            detail16Pro_4
+        ]
+    },
+    ip16e: {
+        images: [
+            detail16e_1,
+            detail16e_2,
+            detail16e_3,
+            detail16e_4,
+            detail16e_5
+        ]
+    }
+}
+
+// List of iPhone models
+
+import ip16All from "../assets/product/item/ip16/all/xlarge.jpg"
+import ip16All_small from "../assets/product/item/ip16/all/small.jpg";
+import ip16Ultramarine from "../assets/product/item/ip16/ultramarine/xlarge.jpg";
+import ip16Ultramarine_small from "../assets/product/item/ip16/ultramarine/small.jpg";
+import ip16Teal from "../assets/product/item/ip16/teal/xlarge.jpg";
+import ip16Teal_small from "../assets/product/item/ip16/teal/small.jpg";
+import ip16Black from "../assets/product/item/ip16/black/xlarge.jpg";
+import ip16Black_small from "../assets/product/item/ip16/black/small.jpg";
+import ip16White from "../assets/product/item/ip16/white/xlarge.jpg";
+import ip16White_small from "../assets/product/item/ip16/white/small.jpg";
+
+import ip16ProAll from "../assets/product/item/ip16pro/all/xlarge.jpg";
+import ip16ProAll_small from "../assets/product/item/ip16pro/all/small.jpg";
+import ip16ProBlack from "../assets/product/item/ip16pro/black/xlarge.jpg";
+import ip16ProBlack_small from "../assets/product/item/ip16pro/black/small.jpg";
+import ip16ProWhite from "../assets/product/item/ip16pro/white/xlarge.jpg";
+import ip16ProWhite_small from "../assets/product/item/ip16pro/white/small.jpg";
+import ip16ProGold from "../assets/product/item/ip16pro/golden/xlarge.jpg";
+import ip16ProGold_small from "../assets/product/item/ip16pro/golden/small.jpg";
+import ip16ProSilver from "../assets/product/item/ip16pro/gray/xlarge.jpg";
+import ip16ProSilver_small from "../assets/product/item/ip16pro/gray/small.jpg";
+
+
+export const productListImages = {
+    ip16: {
+        all: {
+            hero: ip16All,
+            small: ip16All_small
+        },
+        ultramarine: {
+            hero: ip16Ultramarine,
+            small: ip16Ultramarine_small
+        },
+        teal: {
+            hero: ip16Teal,
+            small: ip16Teal_small
+        },
+        black: {
+            hero: ip16Black,
+            small: ip16Black_small
+        },
+        white: {
+            hero: ip16White,
+            small: ip16White_small
+        }
+    },
+    ip16Pro: {
+        all: {
+            hero: ip16ProAll,
+            small: ip16ProAll_small
+        },
+        black: {
+            hero: ip16ProBlack,
+            small: ip16ProBlack_small
+        },
+        white: {
+            hero: ip16ProWhite,
+            small: ip16ProWhite_small
+        },
+        gold: {
+            hero: ip16ProGold,
+            small: ip16ProGold_small
+        },
+        silver: {
+            hero: ip16ProSilver,
+            small: ip16ProSilver_small
+        }
+    }
+}
+
+// AI images
+
+import ai16 from "../assets/product/ai/ip16/xlarge.jpg";
+import ai16_small from "../assets/product/ai/ip16/small.jpg";
+
+import ai16Pro from "../assets/product/ai/ip16pro/xlarge.jpg";
+import ai16Pro_small from "../assets/product/ai/ip16pro/small.jpg";
+
+import ai16e_frame from "../assets/product/ai/ip16e/frame_xlarge.jpg";
+import ai16e_frame_small from "../assets/product/ai/ip16e/frame_small.jpg";
+import ai16e_screen from "../assets/product/ai/ip16e/screen_xlarge.jpg";
+import ai16e_screen_small from "../assets/product/ai/ip16e/screen_small.jpg";
+
+export const productAIImages = {
+    ip16: {
+        hero: ai16,
+        small: ai16_small
+    },
+    ip16Pro: {
+        hero: ai16Pro,
+        small: ai16Pro_small
+    },
+    ip16e: {
+        frame: {
+            hero: ai16e_frame,
+            small: ai16e_frame_small
+        },
+        screen: {
+            hero: ai16e_screen,
+            small: ai16e_screen_small
+        }
+    }
+}
+
+// Visual 
+import visual16 from "../assets/product/visual/ip16/large.jpg"
+import visual16pro from "../assets/product/visual/ip16pro/large.jpg"
+import visual16e from "../assets/product/visual/ip16e/large.jpg"
+
+export const productVisualImages = {
+    ip16: visual16,
+    ip16Pro: visual16pro,
+    ip16e: visual16e
+}
