fc5cb748ada2a21727a8b81e7acf588d7bc814db
TLam2908 <buitunglam2003@gmail.com>
2025-06-07 22:49:48 +0700

    Update productRegistration.tsx

--- client/src/components/product/productRegistration.tsx
+++ client/src/components/product/productRegistration.tsx
@@ -5,7 +5,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
 
